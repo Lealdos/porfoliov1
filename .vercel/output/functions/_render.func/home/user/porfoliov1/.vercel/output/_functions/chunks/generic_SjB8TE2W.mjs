0921ff50ec1798c5f2ae447cd8f075fcd7af00a2
Jose Delgado <j3leonardodelgado@gmail.com>
2024-05-28 23:32:43 -0400

    static vercel

--- .vercel/output/functions/_render.func/home/user/porfoliov1/.vercel/output/_functions/chunks/generic_SjB8TE2W.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_X_NGTUzW.mjs');
-
-export { page };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import robotsTxt from "astro-robots-txt";
+import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  output: "server",
-  adapter: vercel()
+    integrations: [tailwind(), robotsTxt()],
+    output: 'static',
+    adapter: vercelStatic(),
 });
