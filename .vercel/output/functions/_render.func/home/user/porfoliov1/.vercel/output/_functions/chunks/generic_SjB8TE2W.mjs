f7cc23b9b0436d11285623ed8cc6559ec99a0194
j3leonardodelgado <j3leonardodelgado@gmail.com>
2024-05-29 02:23:33 +0000

    first commit

---
+++ .vercel/output/functions/_render.func/home/user/porfoliov1/.vercel/output/_functions/chunks/generic_SjB8TE2W.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_X_NGTUzW.mjs');
+
+export { page };
---
+++ .vercel/output/functions/_render.func/home/user/porfoliov1/.vercel/output/_functions/chunks/index_CEjCKIis.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_1k9cKYF9.mjs');
+
+export { page };
