9d51eadebf3d2142a27e59eb78501c86bd725a90
jose L Delgado Avila <105816982+Lealdos@users.noreply.github.com>
2024-08-28 23:41:30 -0400

    fixed: vercel deployment on astro.config.msj

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
 
-import vercelStatic from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), robotsTxt()],
-    output: 'static',
-    adapter: vercelStatic(),
+    output: 'server',
+    adapter: vercel(),
 });
